e2cbeb5ed7115683f6a45f1daff5df62f813cd9c
HuongNguyen <nguyenhuong150905@gmail.com>
2025-11-20 20:29:33 +0700

    reset code

--- server/Authentication_Service/obj/Debug/net8.0/Authentication_Service.AssemblyInfo.cs
+++ server/Authentication_Service/obj/Debug/net8.0/Authentication_Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d514e53b80d43c6367e7c5e7d9026e23a89e138d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c2566cae7d6546a50409d37bec28b88f3cb62d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
